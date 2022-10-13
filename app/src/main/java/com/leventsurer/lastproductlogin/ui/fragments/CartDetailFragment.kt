8a0bdc5d42b5c6ff25bca7fe48ae594c55532f89
Levent Sürere <srrlevent@gmail.com>
2022-10-13 14:34:40 +0300

    ekran görüntüsü eklendi

--- app/src/main/java/com/leventsurer/lastproductlogin/ui/fragments/CartDetailFragment.kt
+++ app/src/main/java/com/leventsurer/lastproductlogin/ui/fragments/CartDetailFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.leventsurer.lastproductlogin.databinding.FragmentCartDetailBinding
@@ -26,7 +28,6 @@ class CartDetailFragment : Fragment() {
     private var adapterList = ArrayList<ProductDetail>()
     private lateinit var adapter: CartDetailAdapter
     private val mainActivityViewModel: MainActivityViewModel by viewModels()
-    private var totalPrice = 0.0
     private lateinit var chosenCart: Carts
     private var quantityList=ArrayList<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,18 +64,20 @@ class CartDetailFragment : Fragment() {
         valueBind()
     }
 
-    var i = 0
     //private var modelList=ArrayList<CartDetailProductInfo>()
     private fun subscribeObserve() {
 
-        mainActivityViewModel.productDetail.observe(viewLifecycleOwner) {
+        mainActivityViewModel.productDetail.observe(viewLifecycleOwner, Observer {
 
             adapterList.add(it)
             Log.e("listtt","${it.id}")
 
+
             val sortedList = sortIncreasingById(adapterList)
+
             adapter.list = sortedList
-        }
+        })
+
 
 
     }
@@ -106,7 +109,8 @@ class CartDetailFragment : Fragment() {
     private fun valueBind() {
         binding.cartDetailDate.text = modifyDateLayout(chosenCart.date!!)[0]
         binding.cartDetailTime.text = modifyDateLayout(chosenCart.date!!)[1]
-        binding.cartDetailTotalPrice.text = totalPrice.toString()
+
+       //binding.cartDetailTotalPrice.text =totalPrice.toString()
     }
 
     private fun setupAdapter() {
